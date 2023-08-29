aea1758829c9e0365df319c9335e112263ccb8a5
Joan Janku <joan.janku@gmail.com>
2023-08-29 16:42:43 +0200

    proxy

--- src/structural/proxy/SecurityProxyDemo.java
+++ src/structural/proxy/SecurityProxyDemo.java
@@ -4,7 +4,7 @@ public class SecurityProxyDemo {
     public static void main(String[] args) {
         TokenValidator tokenValidator = new TokenValidator();
         SessionTokens sessionTokens = new SessionTokens();
-        String userName = "Andrzej";
+        String userName = "Joan";
         String channelName = "design_patterns";
         String message = "I will learn what proxy is!";
         sessionTokens.createTokenForUser(userName); // supozojme useri beni login
